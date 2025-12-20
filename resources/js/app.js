92368acb3e7b0778b79123b0ef277598e6540448
Pablo Pita <ppitacaneiro@gmail.com>
2025-12-20 18:06:06 +0100

    Add primeicons dependency and import in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@ import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
+import 'primeicons/primeicons.css';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
