8f85b83e7d54726864d0b5543b4fd9879da6109f
Pablo Pita <ppitacaneiro@gmail.com>
2025-12-22 21:21:00 +0100

    Refactor TwilioWebhookController to use TwilioWebhookRequest for validation and create TwilioWebhookRequest class for structured request handling

--- app/Http/Controllers/TwilioWebhookController.php
+++ app/Http/Controllers/TwilioWebhookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Services\TwilioService;
 use Illuminate\Http\Request;
+use App\Http\Requests\TwilioWebhookRequest;
 
 class TwilioWebhookController extends Controller
 {
@@ -11,11 +12,11 @@ class TwilioWebhookController extends Controller
     {
     }
 
-    public function handle(Request $request)
+    public function handle(TwilioWebhookRequest $request)
     {
         \Log::info('Received Twilio webhook:', $request->all());
 
-        $this->twilioService->handleIncomingWhatsAppMessage($request->all());
+        $this->twilioService->handleIncomingWhatsAppMessage($request->validated());
         
         return response('Webhook received', 200);
     }
---
+++ app/Http/Requests/TwilioWebhookRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TwilioWebhookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'From'       => 'required|string',
+            'To'         => 'required|string',
+            'Body'       => 'required|string',
+            'MessageSid' => 'required|string',
+        ];
+    }
+
+    protected function failedValidation(\Illuminate\Contracts\Validation\Validator $validator)
+    {
+        \Log::channel('whatsapp_ticket')->warning(
+            'Invalid Twilio webhook payload',
+            $validator->errors()->toArray()
+        );
+
+        response()->json(['status' => 'ok'], 200)->send();
+        exit;
+    }
+}
