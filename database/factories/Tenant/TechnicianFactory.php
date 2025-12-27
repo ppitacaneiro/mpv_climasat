995dd82a61b62b1570b5e7f226e979ca3254f282
Pablo Pita <ppitacaneiro@gmail.com>
2025-12-27 19:03:09 +0100

    Refactor TechnicianFactory to create and save User model instances for technicians

--- database/factories/Tenant/TechnicianFactory.php
+++ database/factories/Tenant/TechnicianFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories\Tenant;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Tenant\Technician;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tenant\Technician>
@@ -20,13 +21,23 @@ class TechnicianFactory extends Factory
     public function definition(): array
     {
         $availabilities = ['available', 'busy', 'off'];
+        $email = $this->faker->unique()->safeEmail();
+        $name = $this->faker->name();
+        $user = new User([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt('password'), // Default password
+            'role' => 'technician',
+        ]);
+        $user->save();
 
         return [
-            'name' => $this->faker->name(),
+            'name' => $name,
+            'user_id' => $user->id,
             'tax_id' => $this->faker->unique()->bothify('??########'),
             'specialty' => $this->faker->word(),
             'phone' => $this->faker->phoneNumber(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'email' => $email,
             'availability' => $this->faker->randomElement($availabilities),
         ];
     }
